dbe3466a81798e2867c6eadb928d4b8c07cbe6a5
mohsen6006666 <146470443+mohsen6006666@users.noreply.github.com>
2025-04-05 17:19:53 +0530

    Update user_logger.py

--- user_logger.py
+++ user_logger.py
@@ -1,4 +1,4 @@
-# user_logger.py
+# movie_bot/user_logger.py
 
 logged_users = set()
 
@@ -6,12 +6,14 @@ def log_user(update, context):
     user = update.effective_user
     user_id = user.id
     name = user.full_name
-    log_channel_id = -1002699774923  # Replace with your channel ID if needed
+    log_channel_id = -1002699774923  # your Telegram private channel ID
 
     if user_id not in logged_users:
         logged_users.add(user_id)
-
-        context.bot.send_message(
-            chat_id=log_channel_id,
-            text=f"New user: {name} (ID: {user_id})"
-        )
+        try:
+            context.bot.send_message(
+                chat_id=log_channel_id,
+                text=f"New user: {name} (ID: {user_id})"
+            )
+        except Exception as e:
+            print(f"Logging failed: {e}")
